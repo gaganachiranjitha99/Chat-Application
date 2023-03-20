8356a70daa3990076c769ce46b748a2b29fcdb5e
Gagana Chiranjitha <gaganachiranjithacoc2@gmail.com>
2023-03-20 22:42:30 +0530

    update db connection

---
+++ RMI_chatapp/src/dbmanager/DBManager.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dbmanager;
+
+/**
+ *
+ * @author MSI
+ */
+public class DBManager {
+     private static DBManager dbm;
+
+    private DBManager() {
+    }
+
+    static {
+        dbm = new DBManager();
+    }
+
+    public static DBManager getDBM() {
+        return dbm;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
