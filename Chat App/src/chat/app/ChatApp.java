2fbe3478b58d61e24885f6c718b362e760772747
Gagana Chiranjitha <gaganachiranjithacoc2@gmail.com>
2023-03-21 01:39:11 +0530

    Initial commit

--- Chat App/src/chat/app/ChatApp.java
+++ Chat App/src/chat/app/ChatApp.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package rmi_chatapp;
+package chat.app;
 
 /**
  *
  * @author MSI
  */
-public class RMI_chatapp {
+public class ChatApp {
 
     /**
      * @param args the command line arguments
