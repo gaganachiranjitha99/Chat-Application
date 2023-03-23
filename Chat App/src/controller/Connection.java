6a61534adcf44ee43b5fa1c7f81172884e066f98
Gagana Chiranjitha <gaganachiranjithacoc2@gmail.com>
2023-03-23 11:13:11 +0530

    controller and db manager update

---
+++ Chat App/src/controller/Connection.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+/**
+ *
+ * @author MSI
+ */
+public class Connection {
+    
+     private static final SessionFactory sessionFactory;
+    
+    static {
+        try {
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
